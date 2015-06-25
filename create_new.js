b67253c60f18abadc46e93db2935d2de84028f98
Andrew Bowles <Andrew_Bowles@McAfee.com>
2015-06-25 13:55:42 -0600

    'Attempting to pull data from the input locations....It's not working very well. :('

--- create_new.js
+++ create_new.js
@@ -213,3 +213,120 @@ function create_new_blocker_small() {
   new_blocker.appendChild(new_blocker_id);
   new_blocker_id.textContent = "ID: " + new_blocker.id;
 }
+
+
+
+
+function put_new_task (argument) {
+   // body...
+ }
+function put_new_owner (argument) {
+   // body...
+ }
+function put_new_story (argument) {
+   // body...
+ }
+function put_new_dependancy (argument) {
+   // body...
+ }
+function put_new_blocker (argument) {
+   // body...
+ }
+function put_new_column (argument) {
+   // body...
+ }
+
+// =================================================================
+/*
+function create_new_task_small() {
+  // alert("test!");
+  var new_task = document.createElement('div');
+  new_task.id = "task_" + task_id;
+  task_id++;
+  // alert(new_task.id + ", " + task_id);
+  new_task.className = 'task_note_small';
+  var candragya = document.createAttribute("draggable");
+  var dragstart = document.createAttribute("ondragstart");
+  candragya.value = "true";
+  dragstart.value = "drag(event)";
+// isdraggable
+// isdragstart
+  new_task.setAttributeNode(candragya);
+  new_task.setAttributeNode(dragstart);
+  // document.getElementById("note_holder").appendChild(new_task);
+  document.getElementById("div1").appendChild(new_task);
+  var new_task_id = document.createElement('p');
+  new_task.appendChild(new_task_id);
+  new_task_id.textContent = "ID: " + new_task.id;
+}
+
+function create_new_story_small() {
+  // alert("test!");
+  var new_story = document.createElement('div');
+  new_story.id = "story_" + story_id;
+  story_id++;
+  new_story.className = 'story_note_small';
+  var candragya = document.createAttribute("draggable");
+  var dragstart = document.createAttribute("ondragstart");
+  candragya.value = "true";
+  dragstart.value = "drag(event)";
+// isdraggable
+// isdragstart
+  new_story.setAttributeNode(candragya);
+  new_story.setAttributeNode(dragstart);
+  // document.getElementById("note_holder").appendChild(new_story);
+  document.getElementById("div1").appendChild(new_story);
+  var new_story_id = document.createElement('p');
+  new_story.appendChild(new_story_id);
+  new_story_id.textContent = "ID: " + new_story.id;
+}
+
+//}
+
+function create_new_dependancy_small() {
+  // alert("test!");
+  var new_dependancy = document.createElement('div');
+  new_dependancy.id = "dependancy_" + dependancy_id;
+  dependancy_id++;
+  // alert(new_dependancy.id + ", " + dependancy_id);
+  new_dependancy.className = 'dependancy_note_small';
+  var candragya = document.createAttribute("draggable");
+  var dragstart = document.createAttribute("ondragstart");
+  candragya.value = "true";
+  dragstart.value = "drag(event)";
+// isdraggable
+// isdragstart
+  new_dependancy.setAttributeNode(candragya);
+  new_dependancy.setAttributeNode(dragstart);
+  // get element id based on where it needs to be and what it needs to be attached to.
+  document.getElementById("div1").appendChild(new_dependancy);
+  var new_dependancy_id = document.createElement('p');
+  new_dependancy.appendChild(new_dependancy_id);
+  new_dependancy_id.textContent = "ID: " + new_dependancy.id;
+}
+
+function create_new_blocker_small() {
+  // alert("test!");
+  var new_blocker = document.createElement('div');
+  new_blocker.id = "blocker_" + blocker_id;
+  blocker_id++;
+  // alert(new_blocker.id + ", " + blocker_id);
+  new_blocker.className = 'blocker_note_small';
+  var candragya = document.createAttribute("draggable");
+  var dragstart = document.createAttribute("ondragstart");
+  candragya.value = "true";
+  dragstart.value = "drag(event)";
+// isdraggable
+// isdragstart
+  new_blocker.setAttributeNode(candragya);
+  new_blocker.setAttributeNode(dragstart);
+  // Place according to the needed location.
+  document.getElementById("div1").appendChild(new_blocker);
+  var new_blocker_id = document.createElement('p');
+  new_blocker.appendChild(new_blocker_id);
+  new_blocker_id.textContent = "ID: " + new_blocker.id;
+}
+*/
+
+// =============================================================================
+
